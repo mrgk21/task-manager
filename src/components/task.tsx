0806e8e4ad3b18b6b28bd2a94336257e50314d4e
Gaurav Kelkar <gauravkelkar20@gmail.com>
2023-07-16 20:28:01 +0530

    dev: added create task page

--- src/components/task.tsx
+++ src/components/task.tsx
@@ -11,34 +11,49 @@ interface Props {
 
 const Task = ({ task: { _id, description, title }, index }: Props) => {
 	const [isCompleted, setIsCompleted] = useState(false);
+	const [readMore, setReadMore] = useState(false);
 	return (
 		<Draggable draggableId={_id} index={index}>
 			{(provided) => (
 				<div ref={provided.innerRef} {...provided.draggableProps}>
-					<div className="flex items-center p-2 px-5 space-x-3 dark:border-black border w-[500px] h-[50px] rounded-md shadow-lg ">
-						<span {...provided.dragHandleProps}>
-							<FontAwesomeIcon icon={faBars} />
+					<div className="flex items-center p-2 px-5 space-x-3 dark:border-black border w-[500px] rounded-md shadow-lg ">
+						<span {...provided.dragHandleProps} className="mt-2.5 self-start w-[5%]">
+							<FontAwesomeIcon icon={faBars} size="lg" />
 						</span>
-						<div className="flex flex-grow  items-center">
-							<span className="text-xl">{title}</span>
-							<div className="space-x-2 ml-auto">
-								{isCompleted ? (
-									<FontAwesomeIcon
-										icon={faCircleCheck}
-										className="text-green-500 hover:cursor-pointer w-[20px] text-center"
-										onClick={() => setIsCompleted(false)}
-									/>
-								) : (
-									<FontAwesomeIcon
-										icon={faExclamation}
-										className="text-yellow-500 hover:cursor-pointer w-[20px] text-center"
-										onClick={() => setIsCompleted(true)}
-									/>
-								)}
-								<span className="hover:text-red-500 hover:cursor-pointer">
-									<FontAwesomeIcon icon={faTrash} />
-								</span>
+						<div className="flex flex-col w-[95%]">
+							<div className="flex items-center mt-2">
+								<span className="text-xl">{title}</span>
+								<div className="space-x-2 ml-auto">
+									{isCompleted ? (
+										<FontAwesomeIcon
+											icon={faCircleCheck}
+											className="text-green-500 hover:cursor-pointer w-[20px] text-center"
+											onClick={() => setIsCompleted(false)}
+										/>
+									) : (
+										<FontAwesomeIcon
+											icon={faExclamation}
+											className="text-yellow-500 hover:cursor-pointer w-[20px] text-center"
+											onClick={() => setIsCompleted(true)}
+										/>
+									)}
+									<span className="hover:text-red-500 hover:cursor-pointer">
+										<FontAwesomeIcon icon={faTrash} />
+									</span>
+								</div>
 							</div>
+							<p className="text-sm line-clamp-3">
+								{description}
+								Lorem Ipsum is simply dummy text of the printing and typesetting
+								industry. Lorem Ipsum has been the industrys standard dummy text
+								ever since the 1500s, when an unknown printer took a galley of type
+								and scrambled it to make a type specimen book. It has survived not
+								only five centuries, but also the leap into electronic typesetting,
+								remaining essentially unchanged. It was popularised in the 1960s
+								with the release of Letraset sheets containing Lorem Ipsum passages,
+								and more recently with desktop publishing software like Aldus
+								PageMaker including versions of Lorem Ipsum.
+							</p>
 						</div>
 					</div>
 				</div>
--- src/pages/tasks/create.tsx
+++ src/pages/tasks/create.tsx
@@ -1,5 +1,102 @@
+import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { Link } from "react-router-dom";
+import * as yup from "yup";
+
+interface CustomElements extends HTMLFormControlsCollection {
+	title: HTMLInputElement;
+	description: HTMLInputElement;
+}
+
+interface TaskForm extends HTMLFormElement {
+	readonly elements: CustomElements;
+}
+
+const taskSchema = yup.object().shape({
+	title: yup.string().min(3).defined().required(),
+	description: yup.string().min(3).defined().required(),
+});
+
 const CreateTask = () => {
-	return <div>Hello from create</div>;
+	const [errors, setErrors] = useState<{ [k: string]: string | null }>({});
+
+	const handleSubmit = async (event: FormEvent<TaskForm>) => {
+		event.preventDefault();
+		const { title, description } = event.currentTarget.elements;
+
+		try {
+			const result = await taskSchema.validate(
+				{
+					title: title.value,
+					description: description.value,
+				},
+				{ abortEarly: false },
+			);
+			console.log(result);
+		} catch (error: unknown) {
+			const errs: typeof errors = {};
+
+			if (error instanceof yup.ValidationError) {
+				error.inner.forEach((item) => {
+					errs[item.path as string] = item.message;
+				});
+				setErrors(errs);
+				return;
+			}
+		}
+	};
+
+	const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+		const errs = { ...errors };
+		errs[event.target.name] = null;
+		setErrors(errs);
+	};
+
+	return (
+		<div className="mx-auto max-w-lg space-y-2">
+			<h1 className="text-2xl">Create a new task</h1>
+			<form onSubmit={handleSubmit} className="flex flex-col space-y-2">
+				<div className="flex flex-col">
+					<label htmlFor="task_title">Enter a title</label>
+					{errors.title && (
+						<label htmlFor="task_title" className="text-red-500 text-sm">
+							{errors.title}
+						</label>
+					)}
+					<input
+						type="text"
+						name="title"
+						id="task_title"
+						className="p-1"
+						onChange={handleChange}
+					/>
+				</div>
+
+				<div className="flex flex-col">
+					<label htmlFor="task_description">Enter a description</label>
+					{errors.description && (
+						<label htmlFor="task_description" className="text-red-500 text-sm">
+							{errors.description}
+						</label>
+					)}
+					<input
+						type="text"
+						name="description"
+						id="task_description"
+						className="p-1"
+						onChange={handleChange}
+					/>
+				</div>
+				<button type="submit" className="border border-black p-2 rounded-md">
+					Create a task
+				</button>
+			</form>
+			<Link to="/tasks" tabIndex={0} className="underline">
+				<FontAwesomeIcon icon={faArrowLeftLong} /> Go back to tasks
+			</Link>
+		</div>
+	);
 };
 
 export default CreateTask;
