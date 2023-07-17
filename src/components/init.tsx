d6a1f34293f91afd4ef4db1db517fcd04d839795
Gaurav Kelkar <gauravkelkar20@gmail.com>
2023-07-17 11:26:33 +0530

    dev: added redux persist

--- src/components/init.tsx
+++ src/components/init.tsx
@@ -1,12 +1,18 @@
 import { ReactNode } from "react";
 import { Provider as ReduxProvider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 import store from "../feaures/store";
 
 interface Props {
 	children: ReactNode;
 }
 const Init = ({ children }: Props) => {
-	return <ReduxProvider store={store}>{children}</ReduxProvider>;
+	return (
+		<ReduxProvider store={store}>
+			<PersistGate persistor={persistStore(store)}>{children}</PersistGate>
+		</ReduxProvider>
+	);
 };
 
 export default Init;
--- src/feaures/store.ts
+++ src/feaures/store.ts
@@ -1,8 +1,16 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { baseApi } from "./api";
 import { authReducer } from "./auth/auth.slice";
 import { taskGroupReducer } from "./taskGroup/taskGroup.slice";
+
+const persistConfig = {
+	key: "root",
+	storage,
+	blacklist: ["auth"],
+};
 
 const reducer = combineReducers({
 	[baseApi.reducerPath]: baseApi.reducer,
@@ -10,9 +18,16 @@ const reducer = combineReducers({
 	taskGroup: taskGroupReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 const store = configureStore({
-	reducer,
-	middleware: (gDM) => gDM().concat(baseApi.middleware),
+	reducer: persistedReducer,
+	middleware: (gDM) =>
+		gDM({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}).concat(baseApi.middleware),
 });
 
 setupListeners(store.dispatch);
