43066bcb8b5d2948c6a3ec6ee1e1e7148e4c9ffc
Gaurav Kelkar <gauravkelkar20@gmail.com>
2023-07-20 15:47:54 +0530

    refactor: code cleanup

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,7 +2,6 @@ import { useRouteError } from "react-router-dom";
 
 const Error404 = () => {
 	const error = useRouteError() as Error;
-	console.error(typeof error);
 
 	return <div>{error.message}</div>;
 };
